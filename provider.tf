0a5573f409d1528fc8d4504f4ccdb6dbce78d2d4
Tatiana Rotaru <tatiana.zubco11@gmail.com>
2023-12-02 12:45:11 -0600

    added provider

--- provider.tf
+++ provider.tf
@@ -1,15 +1,33 @@
-# Azure Provider source and version being used
+# Specify Terraform Provider and Version
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.82.0"
+      version = "3.81.0"
     }
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  skip_provider_registration = true 
+  # Skip automatic provider registration
+  skip_provider_registration = true
   features {}
+}
+
+# ---------------------------------------------------------------------------
+
+# Configure the AWS account
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "YOUR-ACCESS-KEY"
+  secret_key = "YOUR-SECRET-KEY"
+}
+
+# AWS Route 53
+resource "aws_route53_record" "wordpressdb" {
+  zone_id = "YOUR-ZONE-ID"
+  name    = "wordpress.YOUR-DOMAIN.net"
+  type    = "A"
+  ttl     = 300
+  records = [azurerm_public_ip.example.ip_address]
 }
